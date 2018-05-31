df26b0102a46cd541adbfe59f9625bac21d74437
Rafael Chicoli <rafaelchicoli@hotmail.com>
2018-05-31 17:39:16 +0200

    add processor

---
+++ cmd/processor/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
+)
+
+var (
+
+	// ErrNameNotProvided is thrown when a name is not provided
+	ErrNameNotProvided = errors.New("no name was provided in the HTTP body")
+
+	// ErrBadRequest ...
+	ErrBadRequest = errors.New(http.StatusText(http.StatusBadRequest))
+)
+
+func main() {
+
+	// Initialize a session in us-west-2 that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials.
+	sess, err := session.NewSession(&aws.Config{Region: aws.String("eu-west-1")})
+	if err != nil {
+		log.Fatalf("wrong session: %v", err)
+	}
+
+	kc := &KinesisService{
+		client:      kinesis.New(sess),
+		requestPool: make([]*kinesis.PutRecordsRequestEntry, 100),
+		streamName:  "rc-playground",
+		shards:      10,
+	}
+
+	lambda.Start(kc.Handler)
+}
+
+type KinesisService struct {
+	client      *kinesis.Kinesis
+	streamName  string
+	shards      int
+	requestPool []*kinesis.PutRecordsRequestEntry
+}
+
+// Handler is your Lambda function handler
+// It uses Amazon API Gateway request/responses provided by the aws-lambda-go/events package,
+// However you could use other event sources (S3, Kinesis etc), or JSON-decoded primitive types such as 'string'.
+func (kc *KinesisService) Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
+
+	// If no name is provided in the HTTP request body, throw an error
+	if len(request.Body) < 1 {
+		return response("", http.StatusBadRequest, ErrNameNotProvided)
+	}
+
+	rdn := strconv.Itoa(rand.Intn(kc.shards))
+
+	_, err := kc.client.PutRecord(&kinesis.PutRecordInput{
+		Data:         []byte(request.Body),
+		StreamName:   aws.String(kc.streamName),
+		PartitionKey: aws.String(rdn),
+	})
+	if err != nil {
+		return response("could not put records into kinesis stream", http.StatusInternalServerError, err)
+	}
+
+	return response("OK", http.StatusOK, nil)
+
+}
+
+func response(body string, statusCode int, err error) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       body,
+		StatusCode: statusCode,
+	}, err
+}
